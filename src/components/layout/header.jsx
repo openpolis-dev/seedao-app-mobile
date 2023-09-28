f1977bd39b422093606bd2e9be6c6e4958f18b15
sr <7518647+wendychaung@users.noreply.github.com>
2023-09-28 23:04:59 +0800

    Update header.jsx

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import {ChevronLeft} from "react-bootstrap-icons";
 import {Row,Col} from "react-bootstrap";
 import Loading from "./loading";
+import {useNavigate} from "react-router-dom";
+
 
 const Box = styled.div`
     position: fixed;
@@ -22,9 +24,15 @@ const Mid = styled(Col)`
     text-align: center;
 `
 export default function Header({title}){
+    const navigate = useNavigate();
+
+    const backTop = () =>{
+        navigate(-1)
+    }
+
     return <Box>
         <Row>
-            <Col xs={2}><ChevronLeft /></Col>
+            <Col xs={2} onClick={()=>backTop()}><ChevronLeft /></Col>
             <Mid xs={8}>{title}</Mid>
             <Rht xs={2}>
                 <Loading/>
