fd0be36437132cfab42ad321c7e6c7c8debafefb
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-09-29 23:16:23 +0800

    add tab component

---
+++ src/components/common/tab.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export default function Tab({ data, onChangeTab, value, ...rest }) {
+  return (
+    <TabMenu {...rest}>
+      {data &&
+        data.map((item, idx) => (
+          <li key={idx} onClick={() => onChangeTab(item.value)} className={value === item.value ? "selected" : ""}>
+            {item.label}
+          </li>
+        ))}
+    </TabMenu>
+  );
+}
+
+const TabMenu = styled.ul`
+  display: flex;
+  height: 40px;
+  line-height: 40px;
+  color: var(--bs-primary);
+  li {
+    padding-inline: 20px;
+    text-align: center;
+    &.selected {
+      border-bottom: 3px solid var(--bs-primary);
+    }
+  }
+`;
