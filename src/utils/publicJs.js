11503f307de8e6e8497db44e3f2ea4deac1b79b6
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-09-30 17:58:45 +0800

    modify the publicjs

--- src/utils/publicJs.js
+++ src/utils/publicJs.js
@@ -1,17 +1,17 @@
-
-const AddressToShow = address => {
+const AddressToShow = (address) => {
   if (!address) {
-    return '...';
+    return "...";
+  }
+  if (address.length <= 8) {
+    return address;
   }
 
-  let frontStr = address.substring(0, 4);
+  const frontStr = address.substring(0, 4);
 
-  let afterStr = address.substring(address.length - 4, address.length);
+  const afterStr = address.substring(address.length - 4, address.length);
 
   return `${frontStr}...${afterStr}`;
 };
-
-
 
 export default {
   AddressToShow,
