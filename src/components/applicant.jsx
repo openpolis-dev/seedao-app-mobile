171a7672f997264c10dd3f5d9cf4fce12436859a
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-09-30 17:59:05 +0800

    add applicant card component

---
+++ src/components/applicant.jsx
@@ -0,0 +1,84 @@
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import { formatNumber } from "utils/number";
+import { formatTime } from "utils/time";
+import publicJs from "utils/publicJs";
+
+const ApplicationStatus = {
+  All: "",
+  Open: "open",
+  Approved: "approved",
+  Rejected: "rejected",
+  Processing: "processing",
+  Completed: "completed",
+};
+
+const formatApplicationStatus = (status) => {
+  switch (status) {
+    case ApplicationStatus.Open:
+      return "Project.ToBeReviewed";
+    case ApplicationStatus.Approved:
+      return "Project.ToBeIssued";
+    case ApplicationStatus.Rejected:
+      return "Project.Rejected";
+    case ApplicationStatus.Processing:
+      return "Project.Sending";
+    case ApplicationStatus.Completed:
+      return "Project.Sended";
+    default:
+      return "";
+  }
+};
+
+export default function ApplicantCard({ data }) {
+  const { t } = useTranslation();
+  return (
+    <ApplicantCardBox>
+      <Line>
+        <span>{publicJs.AddressToShow(data.target_user_wallet)}</span>
+        <div className="token">
+          {!!data.token_amount && <span>USDT {formatNumber(data.token_amount)}</span>}
+          {!!data.credit_amount && (
+            <span>
+              {t("Project.Points")} {formatNumber(data.credit_amount)}
+            </span>
+          )}
+        </div>
+      </Line>
+      <Line>
+        <span>{formatTime(data.created_at)}</span>
+        <span className="status">{t(formatApplicationStatus(data.status))}</span>
+      </Line>
+    </ApplicantCardBox>
+  );
+}
+
+const ApplicantCardBox = styled.div`
+  width: 100%;
+  background-color: #fff;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  border-radius: 8px;
+  overflow: hidden;
+  padding: 15px;
+  font-size: 14px;
+`;
+
+const Line = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  &:first-child {
+    margin-bottom: 8px;
+  }
+  .status {
+    color: var(--bs-primary);
+    font-weight: 600;
+  }
+  .token span {
+    margin-left: 6px;
+    &:first-child {
+      margin-left: 0;
+    }
+  }
+`;
