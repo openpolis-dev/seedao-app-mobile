712b28b41af82bf3320a5976a64a13b7a886c285
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-09-30 13:43:33 +0800

    hide the scroll bar

--- src/components/common/tab.jsx
+++ src/components/common/tab.jsx
@@ -3,28 +3,38 @@ import styled from "styled-components";
 export default function Tab({ data, onChangeTab, value, ...rest }) {
   return (
     <TabMenu {...rest}>
-      {data &&
-        data.map((item, idx) => (
-          <li key={idx} onClick={() => onChangeTab(item.value)} className={value === item.value ? "selected" : ""}>
-            {item.label}
-          </li>
-        ))}
+      <ul>
+        {data &&
+          data.map((item, idx) => (
+            <li key={idx} onClick={() => onChangeTab(item.value)} className={value === item.value ? "selected" : ""}>
+              {item.label}
+            </li>
+          ))}
+      </ul>
     </TabMenu>
   );
 }
 
-const TabMenu = styled.ul`
+const TabMenu = styled.div`
   width: 100%;
   overflow-x: auto;
-  display: flex;
   height: 40px;
   line-height: 40px;
   color: var(--bs-primary);
+  ul {
+    display: flex;
+    height: 40px;
+  }
   li {
     padding-inline: 20px;
+    white-space: nowrap;
     text-align: center;
     &.selected {
       border-bottom: 3px solid var(--bs-primary);
     }
+  }
+  &::-webkit-scrollbar {
+    width: 0;
+    height: 0;
   }
 `;
