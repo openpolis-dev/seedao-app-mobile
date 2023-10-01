f2d0138fdc97446d36606be8ef3413c7deb1168a
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-01 20:28:22 +0800

    add debug in devlopment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import VConsole from "vconsole";
+
+process.env.NODE_ENV === 'development' && new VConsole();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
