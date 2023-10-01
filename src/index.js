d3ed343e477e29c7471a0f540435f7432ee0469f
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-01 22:02:57 +0800

    add sw register

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import VConsole from "vconsole";
+import * as serviceWorkerRegistration from "./utils/serviceWorkerRegistration";
 
-process.env.NODE_ENV === 'development' && new VConsole();
+process.env.NODE_ENV === "development" && new VConsole();
 
 const appHeight = () => document.documentElement.style.setProperty("--app-height", `${window.innerHeight}px`);
 window.addEventListener("resize", appHeight);
 appHeight();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
+serviceWorkerRegistration.register();
