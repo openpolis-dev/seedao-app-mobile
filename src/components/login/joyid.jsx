7459c92285faa323cdaa37dafe89ece84ee3d0bc
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-03 22:00:27 +0800

    fix text

--- src/components/login/joyid.jsx
+++ src/components/login/joyid.jsx
@@ -144,7 +144,7 @@ export default function Joyid(){
     },[result])
 
     return <div>
-        <Button onClick={()=>onConnectRedirect()}>Joyid</Button>
+        <Button onClick={()=>onConnectRedirect()}>JoyID</Button>
     </div>
 }
 
