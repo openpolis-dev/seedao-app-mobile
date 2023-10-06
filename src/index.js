d2845a3ad09cd069cde064eb55f2ce9bbd4cef75
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-06 12:56:00 +0800

    remove sw code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import VConsole from "vconsole";
-import * as serviceWorkerRegistration from "./utils/serviceWorkerRegistration";
 
 process.env.NODE_ENV === "development" && new VConsole();
 
@@ -12,5 +11,3 @@ appHeight();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
-serviceWorkerRegistration.register();
