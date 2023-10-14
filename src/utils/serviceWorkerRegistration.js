9890bfd8d62c369871859991778b12c13be3803f
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-14 15:19:41 +0800

    fix notice path

--- src/utils/serviceWorkerRegistration.js
+++ src/utils/serviceWorkerRegistration.js
@@ -5,7 +5,7 @@ import { firebaseConfig } from "../firebase-env";
 
 let messaging;
 
-const version = 'v1.0.1';
+const version = 'v1.0.2';
 
 export function register(config) {
   if ('serviceWorker' in navigator) {
