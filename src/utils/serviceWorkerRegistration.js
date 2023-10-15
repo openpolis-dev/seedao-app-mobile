639e4ad3a1bbd58e7ebae1c9cff0fef3ca3f5808
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-16 00:47:07 +0800

    remove dev check when get push token

--- src/utils/serviceWorkerRegistration.js
+++ src/utils/serviceWorkerRegistration.js
@@ -1,20 +1,20 @@
-import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken, onMessage, isSupported } from 'firebase/messaging';
-import { registerDevice, getPushDevice } from 'api/push';
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage, isSupported } from "firebase/messaging";
+import { registerDevice, getPushDevice } from "api/push";
 import { firebaseConfig } from "../firebase-env";
 
 let messaging;
 
-const version = 'v1.0.2';
+const version = "v1.0.2";
 
 export function register(config) {
-  if ('serviceWorker' in navigator) {
+  if ("serviceWorker" in navigator) {
     // Initialize Firebase
     const firebaseApp = initializeApp(firebaseConfig);
     messaging = getMessaging(firebaseApp);
 
-    window.addEventListener('load', () => {
-      registerValidSW('/firebase-messaging-sw.js', config);
+    window.addEventListener("load", () => {
+      registerValidSW("/firebase-messaging-sw.js", config);
     });
   }
 }
@@ -23,26 +23,26 @@ function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
-      if (localStorage.getItem('sw_version') !== version) {
+      if (localStorage.getItem("sw_version") !== version) {
         registration.update().then(function () {
-          localStorage.setItem('sw_version', version);
+          localStorage.setItem("sw_version", version);
         });
       }
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
-        console.log('installingWorker:', installingWorker);
+        console.log("installingWorker:", installingWorker);
         if (installingWorker == null) {
           return;
         }
         installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed') {
+          if (installingWorker.state === "installed") {
             if (navigator.serviceWorker.controller) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
               console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://cra.link/PWA.',
+                "New content is available and will be used when all " +
+                  "tabs for this page are closed. See https://cra.link/PWA.",
               );
 
               // Execute callback
@@ -53,7 +53,7 @@ function registerValidSW(swUrl, config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
+              console.log("Content is cached for offline use.");
               // Execute callback
               if (config?.onSuccess) {
                 config.onSuccess(registration);
@@ -64,12 +64,12 @@ function registerValidSW(swUrl, config) {
       };
     })
     .catch((error) => {
-      console.error('Error during service worker registration:', error);
+      console.error("Error during service worker registration:", error);
     });
 }
 
 export function unregister() {
-  if ('serviceWorker' in navigator) {
+  if ("serviceWorker" in navigator) {
     navigator.serviceWorker.ready
       .then((registration) => {
         registration.unregister();
@@ -85,18 +85,18 @@ export const getPushToken = async () => {
     await navigator.serviceWorker.ready;
     const supported = await isSupported();
     if (!supported) {
-      throw new Error('NOTIFICATIONS_NOT_SUPPORTED');
+      throw new Error("NOTIFICATIONS_NOT_SUPPORTED");
     }
     const token = await getToken(messaging);
     if (token) {
-      console.log('current token for client: ', token);
+      console.log("current token for client: ", token);
       return token;
     } else {
-      throw new Error('No registration token available. Request permission to generate one.');
+      throw new Error("No registration token available. Request permission to generate one.");
     }
   } catch (error) {
-    console.log('An error occurred while retrieving token. ', error);
-    throw new Error('An error occurred while retrieving token.');
+    console.log("An error occurred while retrieving token. ", error);
+    throw new Error("An error occurred while retrieving token.");
   }
 };
 
@@ -115,25 +115,23 @@ export const registerPush = async () => {
       await registerDevice({
         device: getPushDevice(),
         registration_token: deviceToken,
-        language: localStorage.getItem('language') || 'en',
+        language: localStorage.getItem("language") || "en",
       });
     }
   } catch (error) {
     console.error(error);
-    if (process.env.NODE_ENV === 'development') {
-      try {
-        // register push
-        const deviceToken = await getPushToken();
-        if (deviceToken) {
-          await registerDevice({
-            device: getPushDevice(),
-            registration_token: deviceToken,
-            language: localStorage.getItem('language') || 'en',
-          });
-        }
-      } catch (error) {
-        console.error('dev second failed', error);
+    try {
+      // register push
+      const deviceToken = await getPushToken();
+      if (deviceToken) {
+        await registerDevice({
+          device: getPushDevice(),
+          registration_token: deviceToken,
+          language: localStorage.getItem("language") || "en",
+        });
       }
+    } catch (error) {
+      console.error("dev second failed", error);
     }
   }
 };
