e239b06355246de3c8a2aa1e3da7b8a38f66eebb
sr <7518647+wendychaung@users.noreply.github.com>
2023-10-15 14:43:13 +0800

    Update assets.jsx

--- src/pages/assets.jsx
+++ src/pages/assets.jsx
@@ -144,7 +144,6 @@ export default function Assets(){
     const navigate = useNavigate();
 
     const [list, setList] = useState([]);
-    const loading = useSelector(state=> state.loading);
     const [page, setPage] = useState(1);
     const [pageSize, setPageSize] = useState(5);
     const [total, setTotal] = useState(0);
@@ -155,6 +154,12 @@ export default function Assets(){
         floorPrice: 0,
         totalSupply: 0,
     });
+
+    const[status1,setStatus1] = useState(false);
+    const[status2,setStatus2] = useState(false);
+    const[status3,setStatus3] = useState(false);
+    const[status4,setStatus4] = useState(false);
+    const[status5,setStatus5] = useState(false);
 
     const [asset, setAsset] = useState({
         token_remain_amount: 0,
@@ -191,9 +196,14 @@ export default function Assets(){
         getSCR();
     }, []);
 
+    useEffect(()=>{
+        store.dispatch(saveLoading(status1 || status2 || status3 || status4||status5));
+    },[status1,status2,status3,status4,status5])
+
 
     const getAssets = async () => {
-        store.dispatch(saveLoading(true));
+        // store.dispatch(saveLoading(true));
+        setStatus4(true)
         try {
             const res = await getTreasury();
             setAsset({
@@ -207,7 +217,8 @@ export default function Assets(){
         } catch (error) {
             console.error('getTreasury error', error);
         }finally {
-            store.dispatch(saveLoading(false));
+            setStatus4(false)
+            // store.dispatch(saveLoading(false));
         }
     };
 
@@ -215,7 +226,8 @@ export default function Assets(){
         const vaults_map = {};
         const users= [];
         let _total = 0;
-        store.dispatch(saveLoading(true));
+        // store.dispatch(saveLoading(true));
+        setStatus1(true);
         try {
             const reqs = VAULTS.map((item) => getVaultBalance(item));
             const results = await Promise.allSettled(reqs);
@@ -248,7 +260,8 @@ export default function Assets(){
         } catch (error) {
             console.error('getVaultInfo error', error);
         }finally {
-            store.dispatch(saveLoading(false));
+            setStatus1(false);
+            // store.dispatch(saveLoading(false));
         }
         setTotalSigner([...new Set(users)].length);
         setTotalBalance(_total.toFixed(2));
@@ -261,7 +274,8 @@ export default function Assets(){
     }, [page]);
 
     const getRecords = async () => {
-        store.dispatch(saveLoading(true));
+        // store.dispatch(saveLoading(true));
+        setStatus5(true)
         try {
             const res = await getProjectApplications(
                 {
@@ -284,12 +298,15 @@ export default function Assets(){
         } catch (error) {
             console.error('getRecords error', error);
         } finally {
-            store.dispatch(saveLoading(false));
+            setStatus5(false)
+            // store.dispatch(saveLoading(false));
         }
     };
 
 
     const getFloorPrice = async () => {
+        // store.dispatch(saveLoading(true));
+        setStatus3(true)
         try {
             const url = 'https://restapi.nftscan.com/api/v2/statistics/collection/0x23fda8a873e9e46dbe51c78754dddccfbc41cfe1';
             // const {XAPIKEY} = AppConfig;
@@ -304,6 +321,9 @@ export default function Assets(){
             });
         } catch (error) {
             console.error('getFloorPrice error', error);
+        }finally {
+            setStatus3(false)
+            // store.dispatch(saveLoading(false));
         }
     };
 
@@ -312,7 +332,8 @@ export default function Assets(){
         const provider = new ethers.providers.StaticJsonRpcProvider(
             'https://eth-mainnet.g.alchemy.com/v2/YuNeXto27ejHnOIGOwxl2N_cHCfyLyLE',
         );
-        // setLoading1(true)
+        // store.dispatch(saveLoading(true));
+        setStatus2(true)
         try {
             const contract = new ethers.Contract(
                 SCR_CONTRACT,
@@ -338,7 +359,8 @@ export default function Assets(){
         } catch (error) {
             console.error('getSCR error', error);
         }finally {
-            // setLoading1(false)
+            setStatus2(false)
+            // store.dispatch(saveLoading(false));
         }
     };
 
