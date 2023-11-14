6d08bd665dfd8a4b7be790002f2c2df29b673568
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-11-14 15:42:56 +0800

    update governance page

--- src/pages/governance.jsx
+++ src/pages/governance.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import Layout from "components/layout/layout";
 import StickyHeader from "components/layout/StickyHeader";
-import { useTranslation } from "react-i18next";
 import VaultIcon from "assets/Imgs/governance/vault.svg";
 import ProposalIcon from "assets/Imgs/governance/proposal.svg";
-import { Link } from "react-router-dom";
+import GovernanceIcon from "assets/Imgs/governance/governance.svg";
 
 export default function Governance() {
   const { t } = useTranslation();
@@ -28,6 +29,15 @@ export default function Governance() {
             </div>
           </SquareLink>
         </FirstLine>
+        <GovernanceBox>
+          <GovernanceBoxTop>
+            <img src={GovernanceIcon} alt="" />
+            <GovernanceBoxTopLeft>
+              <div className="title">{t("Governance.GovernanceRule")}</div>
+              <div className="desc">{t("Governance.GovernanceRuleDesc")}</div>
+            </GovernanceBoxTopLeft>
+          </GovernanceBoxTop>
+        </GovernanceBox>
       </LayoutContainer>
     </Layout>
   );
@@ -86,4 +96,48 @@ const FirstLineDesc = styled.div`
   color: #ffffff;
   line-height: 21px;
   margin-top: 8px;
+`;
+
+const GovernanceBox = styled.div`
+  width: 100%;
+  height: 247px;
+  background: #ffffff;
+  box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.02);
+  border-radius: 16px;
+  margin-top: 16px;
+  overflow: hidden;
+`;
+
+const GovernanceBoxTop = styled.div`
+  height: 104px;
+  display: flex;
+  justify-content: space-between;
+  background: #ffffff linear-gradient(65deg, #2f76ff 0%, #40c1f8 100%);
+  box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.02);
+  align-items: center;
+  padding-inline: 20px;
+`;
+
+const GovernanceBoxTopLeft = styled.div`
+  text-align: center;
+  .title {
+    font-size: 18px;
+    font-family: Poppins-SemiBold, Poppins;
+    font-weight: 600;
+    color: #ffffff;
+    line-height: 22px;
+    margin-bottom: 9px;
+  }
+  .desc {
+    font-size: 12px;
+    font-family: Poppins-Regular, Poppins;
+    font-weight: 400;
+    color: #ffffff;
+    line-height: 28px;
+    text-align: center;
+    height: 28px;
+    border-radius: 36px;
+    border: 1px solid rgba(255, 255, 255, 0.18);
+    padding-inline: 15px;
+  }
 `;
