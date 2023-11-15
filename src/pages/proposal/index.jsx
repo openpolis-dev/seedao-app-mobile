4f19c4609723af786270af8b9ec17b45acd8eaea
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-11-15 21:15:08 +0800

    update proposal

--- src/pages/proposal/index.jsx
+++ src/pages/proposal/index.jsx
@@ -7,13 +7,13 @@ import { PROPOSAL_CATEGORIES } from "utils/constant";
 import { useState, useEffect } from "react";
 import ProposalSubNav from "components/poposal/proposalSubNav";
 import ProposalCard from "components/poposal/proposalCard";
-import { getAllProposals,getCategories } from "api/proposal";
+import { getAllProposals, getCategories } from "api/proposal";
 import InfiniteScroll from "react-infinite-scroll-component";
 import MsgIcon from "assets/Imgs/msg.png";
 import store from "store";
 import { saveLoading } from "store/reducer";
 import Loading from "components/common/loading";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Proposal() {
   const { t } = useTranslation();
@@ -24,8 +24,7 @@ export default function Proposal() {
   const [orderType, setOrderType] = useState("latest");
   const [activeTab, setActiveTab] = useState(0);
   const [hasMore, setHasMore] = useState(false);
-  const [proposal_categories,setproposal_categories] = useState([])
-
+  const [proposal_categories, setproposal_categories] = useState([]);
 
   useEffect(() => {
     if (activeTab === 0) {
@@ -33,18 +32,17 @@ export default function Proposal() {
     }
   }, [activeTab]);
 
-
   const getCategoriesAPi = async () => {
     // dispatch({ type: AppActionType.SET_LOADING, payload: true });
     try {
       const resp = await getCategories();
-      setproposal_categories(resp?.data.group.categories)
+      setproposal_categories(resp?.data.group.categories);
       // dispatch({
       //   type: AppActionType.SET_PROPOSAL_CATEGORIES,
       //   payload: resp.data.group.categories,
       // });
     } catch (error) {
-      console.error('getCategories failed', error);
+      console.error("getCategories failed", error);
     } finally {
       // dispatch({ type: AppActionType.SET_LOADING, payload: false });
     }
@@ -75,46 +73,40 @@ export default function Proposal() {
     }
   }, [activeTab, orderType]);
   return (
-    <Layout title={t("menus.Proposal")} >
-      {/*<TabMenu>*/}
-      {/*  <li onClick={() => setActiveTab(0)} className={activeTab === 0 ? "selected" : ""}>*/}
-      {/*    {t("Proposal.AllCategories")}*/}
-      {/*  </li>*/}
-      {/*  /!*<li onClick={() => setActiveTab(1)} className={activeTab === 1 ? "selected" : ""}>*!/*/}
-      {/*  /!*  {t("Proposal.TheNeweset")}*!/*/}
-      {/*  /!*</li>*!/*/}
-      {/*</TabMenu>*/}
+    <Layout title={t("Proposal.Governance")}>
       <Content>
         {activeTab === 0 && (
-            <div>
-              {proposal_categories.map((category, index) => (
-                  <CategoryCard key={index}>
-                    <div className="cate-name">
-                      <Link to={`/proposal/category/${category.category_id}`}>{category.name}</Link>
-                    </div>
-                    {!!category.children.length && (
-                        <SubCategoryCard>
-                          {category.children.map((subCategory) => (
-                              <a href={`/proposal/category/${subCategory.category_id}`} key={subCategory.category_id}>
-                                <SubCategoryItem>
-                                  <ImgBox>
-                                    <img src={MsgIcon} alt="" width="24px" height="24px" />
-                                  </ImgBox>
+          <div>
+            {proposal_categories.map((category, index) => (
+              <CategoryCard key={index}>
+                <div className="cate-name">
+                  <Link to={`/proposal/category/${category.category_id}`}>{category.name}</Link>
+                </div>
+                {!!category.children.length && (
+                  <SubCategoryCard>
+                    {category.children.map((subCategory) => (
+                      <a href={`/proposal/category/${subCategory.category_id}`} key={subCategory.category_id}>
+                        <SubCategoryItem>
+                          <ImgBox>
+                            <img src={MsgIcon} alt="" width="24px" height="24px" />
+                          </ImgBox>
 
-                                  <div>
-                                    <div className="name">{subCategory.name}</div>
-                                    <div className="tips">
-                                      <span>{subCategory.thread_count} topics</span>
-                                    </div>
-                                  </div>
-                                </SubCategoryItem>
-                              </a>
-                          ))}
-                        </SubCategoryCard>
-                    )}
-                  </CategoryCard>
-              ))}
-            </div>
+                          <div>
+                            <div className="name">{subCategory.name}</div>
+                            <div className="tips">
+                              <span>
+                                {subCategory.thread_count} {t("Proposal.Topics")}
+                              </span>
+                            </div>
+                          </div>
+                        </SubCategoryItem>
+                      </a>
+                    ))}
+                  </SubCategoryCard>
+                )}
+              </CategoryCard>
+            ))}
+          </div>
         )}
         {activeTab === 1 && (
           <ProposalListContent>
@@ -159,8 +151,6 @@ const Content = styled.div`
   background: var(--background-color);
 `;
 
-
-
 const SubCategoryCard = styled.div`
   border-top: 1px solid #eee;
   display: flex;
@@ -168,7 +158,7 @@ const SubCategoryCard = styled.div`
   flex-wrap: wrap;
   align-items: stretch;
   padding: 10px;
-  a{
+  a {
     display: block;
     width: 50%;
   }
@@ -188,10 +178,10 @@ const SubCategoryItem = styled.div`
     white-space: nowrap;
     text-overflow: ellipsis;
   }
-  .tips{
+  .tips {
     font-size: 12px;
     font-weight: 400;
-    color: #9A9A9A;
+    color: #9a9a9a;
   }
 `;
 
@@ -203,7 +193,7 @@ const CategoryCard = styled.div`
   .cate-name {
     padding-inline: 16px;
     line-height: 40px;
-    
+
     font-size: 16px;
     font-family: Poppins-SemiBold;
     font-weight: 600;
@@ -216,18 +206,17 @@ const ImgBox = styled.div`
   width: 24px;
   height: 24px;
   box-sizing: border-box;
-  background: #5200FF;
+  background: #5200ff;
   border-radius: 100%;
   opacity: 1;
   display: flex;
   align-items: center;
   justify-content: center;
-  img{
+  img {
     width: 14px;
     height: 14px;
   }
-`
-
+`;
 
 const ProposalListContent = styled.div``;
 
