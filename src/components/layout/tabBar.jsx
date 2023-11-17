cd16f1525037f82b3449e4f1e17adff6a971a012
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-11-17 15:42:59 +0800

    format code

--- src/components/layout/tabBar.jsx
+++ src/components/layout/tabBar.jsx
@@ -1,8 +1,8 @@
-import {useEffect} from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
-import {House,Person} from "react-bootstrap-icons";
-import {NavLink} from "react-router-dom"
-import {useTranslation} from "react-i18next";
+import { House, Person } from "react-bootstrap-icons";
+import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 import HomeImg from "../../assets/Imgs/home.svg";
 import HomeHover from "../../assets/Imgs/home_active.png";
@@ -14,7 +14,7 @@ import ExploreImg from "../../assets/Imgs/explore.svg";
 import ExploreHover from "../../assets/Imgs/explore_active.png";
 
 const Box = styled.div`
-  background: rgba(255,255,255,0.6);
+  background: rgba(255, 255, 255, 0.6);
   backdrop-filter: blur(10px);
   -webkit-backdrop-filter: blur(10px);
   position: fixed;
@@ -25,7 +25,7 @@ const Box = styled.div`
   width: 100vw;
   display: flex;
   align-items: center;
-  border-top:1px solid var(--border-color) ;
+  border-top: 1px solid var(--border-color);
 `;
 
 const ItemBox = styled.div`
@@ -33,100 +33,95 @@ const ItemBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  .nor{
+  .nor {
     display: block;
   }
-  .act{
+  .act {
     display: none;
   }
 
-  a{
+  a {
     width: 50%;
-    color:#9A9A9A;
+    color: #9a9a9a;
     display: inline-block;
-    &.active{
+    &.active {
       color: var(--font-color);
-      .nor{
+      .nor {
         display: none;
       }
-      .act{
+      .act {
         display: block;
       }
     }
   }
-    dl{
-
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-    }
-  dt{
-    img{
+  dl {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  dt {
+    img {
       width: 24px;
       margin-bottom: 7px;
     }
   }
- dd{
+  dd {
     font-size: 12px;
   }
-`
+`;
 
-export default function TabBar(){
-    const { t } = useTranslation();
+export default function TabBar() {
+  const { t } = useTranslation();
+  return (
+    <Box>
+      <ItemBox>
+        <NavLink className={({ isActive }) => (isActive ? "active" : "")} to="/governance">
+          <dl>
+            <dt>
+              <img src={GoverImg} className="nor" alt="" />
+              <img src={GoverHover} className="act" alt="" />
+            </dt>
+            <dd>{t("Menus.Governance")}</dd>
+          </dl>
+        </NavLink>
+        <NavLink className={({ isActive }) => (isActive ? "active" : "")} to="/home">
+          <dl>
+            <dt>
+              <img src={HomeImg} className="nor" alt="" />
+              <img src={HomeHover} className="act" alt="" />
+            </dt>
+            <dd>{t("Menus.Square")}</dd>
+          </dl>
+        </NavLink>
 
+        <NavLink className={({ isActive }) => (isActive ? "active" : "")} to="/explore">
+          <dl>
+            <dt>
+              <img src={ExploreImg} className="nor" alt="" />
+              <img src={ExploreHover} className="act" alt="" />
+            </dt>
+            <dd>{t("Menus.Explore")}</dd>
+          </dl>
+        </NavLink>
 
-    // useEffect(() => {
-    //     store.dispatch(saveAccount("123"))
-    // }, []);
-    return <Box>
-        <ItemBox >
-            <NavLink className={({ isActive }) => isActive ?"active":""} to="/governance">
-                <dl>
-                    <dt>
-                        <img src={GoverImg} className="nor" alt=""/>
-                        <img src={GoverHover} className="act" alt=""/>
-                    </dt>
-                    <dd>{t('Menus.Governance')}</dd>
-                </dl>
-            </NavLink>
-            <NavLink className={({ isActive }) => isActive ?"active":""} to="/home">
-                <dl>
-                    <dt>
-                        <img src={HomeImg} className="nor" alt=""/>
-                        <img src={HomeHover} className="act" alt=""/>
-                    </dt>
-                    <dd>{t('Menus.Square')}</dd>
-                </dl>
-            </NavLink>
-
-            <NavLink className={({ isActive }) => isActive ?"active":""} to="/explore">
-                <dl>
-                    <dt>
-                        <img src={ExploreImg} className="nor" alt=""/>
-                        <img src={ExploreHover} className="act" alt=""/>
-                    </dt>
-                    <dd>{t('Menus.Explore')}</dd>
-                </dl>
-            </NavLink>
-
-            {/*<NavLink className={({ isActive }) => isActive ?"active":""} to="/assets">*/}
-            {/*    <dl>*/}
-            {/*        <dt>*/}
-            {/*            <Person />*/}
-            {/*        </dt>*/}
-            {/*        <dd>{t('Menus.assets')}</dd>*/}
-            {/*    </dl>*/}
-            {/*</NavLink>*/}
-            {/*<NavLink className={({ isActive }) => isActive ?"active":""} to="/user/profile">*/}
-            {/*    <dl>*/}
-            {/*        <dt>*/}
-            {/*            <Person />*/}
-            {/*        </dt>*/}
-            {/*        <dd>{t('mobile.my.my')}</dd>*/}
-            {/*    </dl>*/}
-            {/*</NavLink>*/}
-
-        </ItemBox>
+        {/*<NavLink className={({ isActive }) => isActive ?"active":""} to="/assets">*/}
+        {/*    <dl>*/}
+        {/*        <dt>*/}
+        {/*            <Person />*/}
+        {/*        </dt>*/}
+        {/*        <dd>{t('Menus.assets')}</dd>*/}
+        {/*    </dl>*/}
+        {/*</NavLink>*/}
+        {/*<NavLink className={({ isActive }) => isActive ?"active":""} to="/user/profile">*/}
+        {/*    <dl>*/}
+        {/*        <dt>*/}
+        {/*            <Person />*/}
+        {/*        </dt>*/}
+        {/*        <dd>{t('mobile.my.my')}</dd>*/}
+        {/*    </dl>*/}
+        {/*</NavLink>*/}
+      </ItemBox>
     </Box>
+  );
 }
