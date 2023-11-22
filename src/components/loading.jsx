0a322494096fc2ca91081a0df7314a27fd46bd1e
wendychaung <7518647+wendychaung@users.noreply.github.com>
2023-11-22 21:44:42 +0800

    Update loading.jsx

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import LogoImg from "../assets/Imgs/logo.png"
+import {useEffect, useState} from "react";
+import LoadingBox from "components/common/loading";
 
 const Box = styled.div`
     width: 100vw;
@@ -8,10 +10,34 @@ const Box = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  
+  position: fixed;
+  left: 0;
+  top: 0;
 `
 export default function Loading(){
-    return <Box>
-        <img src={LogoImg} alt=""/>
-    </Box>
+
+    const [show,setShow] = useState(false)
+
+
+    useEffect(() => {
+        console.log(window.location.href)
+        if(window.location.href.indexOf("login")>-1){
+            setShow(true)
+        }
+    }, [window.location.href]);
+
+
+    return <>
+        {
+            show && <Box>
+                <img src={LogoImg} alt=""/>
+            </Box>
+        }
+        {
+            !show&& <Box>
+                <LoadingBox />
+            </Box>
+        }
+
+    </>
 }
