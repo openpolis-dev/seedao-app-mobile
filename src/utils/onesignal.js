68e16c1f54355804f0a57e85ff074cf4912acfd6
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-11-23 21:34:31 +0800

    fix init onesignal

--- src/utils/onesignal.js
+++ src/utils/onesignal.js
@@ -1,13 +1,17 @@
-import OneSignal from 'react-onesignal';
-import getConfig from 'constant/envCofnig';
+import OneSignal from "react-onesignal";
+import getConfig from "constant/envCofnig";
 
 export default function runOneSignal() {
+  if (!window.INITIAL_ONESIGNAL) {
+    return;
+  }
   const app_id = getConfig().REACT_APP_ONESIGNAL_ID;
-  console.log('app_id:', app_id);
+  console.log("app_id:", app_id);
   try {
     OneSignal.init({ appId: app_id });
     OneSignal.Slidedown.promptPush();
+    window.INITIAL_ONESIGNAL = true;
   } catch (error) {
-    console.error('init onesignal error:', error);
+    console.error("init onesignal error:", error);
   }
 }
