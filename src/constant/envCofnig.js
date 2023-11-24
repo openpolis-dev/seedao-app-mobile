4e6de4fa3d11ed5a6191cf0eb6a75304ee0f1015
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-11-24 20:02:54 +0800

    modified onesignal config

--- src/constant/envCofnig.js
+++ src/constant/envCofnig.js
@@ -15,23 +15,24 @@ const LOCAL = {
 const DEVELOPMENT = {
   ...LOCAL,
   REACT_APP_ENV: "test",
-  REACT_APP_ONESIGNAL_ID: "2889344b-788a-40d5-8949-1ff1adc71851",
+  REACT_APP_ONESIGNAL_ID: "d3bf95e4-40e3-455d-95ab-e01ef35d6732",
 };
 
 const PREVIEW = {
   ...DEVELOPMENT,
-  REACT_APP_BASE_ENDPOINT: 'https://preview-api.seedao.tech',
+  REACT_APP_BASE_ENDPOINT: "https://preview-api.seedao.tech",
   REACT_APP_THEME_ENABLE: false,
-  REACT_APP_ONESIGNAL_ID: '',
+  REACT_APP_ONESIGNAL_ID: "673e6ac3-ab64-4935-8df7-25dd37baa7d1",
 };
 
 const PRODUCTION = {
   ...LOCAL,
-  REACT_APP_BASE_ENDPOINT: 'https://api.seedao.tech',
-  REACT_APP_PUSH_ENDPOINT: 'https://push-api.seedao.tech',
+  REACT_APP_BASE_ENDPOINT: "https://api.seedao.tech",
+  REACT_APP_PUSH_ENDPOINT: "https://push-api.seedao.tech",
   REACT_APP_JOYID_ENABLE: false,
   REACT_APP_APP_VERSION: `B ${VERSION}`,
   REACT_APP_THEME_ENABLE: false,
+  REACT_APP_ONESIGNAL_ID: "8ecd086b-3e15-4537-9f8b-c55c72a8dcf7",
 };
 
 export default function getConfig() {
