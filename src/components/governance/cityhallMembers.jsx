3e70227b9e9adc269014c250496253c52754a64b
wendychaung <7518647+wendychaung@users.noreply.github.com>
2023-12-06 02:00:44 +0800

    Update cityhallMembers.jsx

--- src/components/governance/cityhallMembers.jsx
+++ src/components/governance/cityhallMembers.jsx
@@ -12,7 +12,7 @@ import Avatar from "components/common/avatar";
 
 const MemberAvatar = ({ user, onSelect }) => {
   const reFormat = () =>{
-    if(user.sns === user.wallet){
+    if(user.sns.toLowerCase() === user.wallet.toLowerCase()){
       return  publicJs.AddressToShow(user.wallet,10)
     }else{
       return user.sns;
