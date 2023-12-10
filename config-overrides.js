820e76f789bd5d16efeedec9c5bbc53f0f186173
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-12-10 16:57:34 +0800

    add split chunk

--- config-overrides.js
+++ config-overrides.js
@@ -28,4 +28,37 @@ const addAnalyzer = () => (config) => {
   return config;
 };
 
-module.exports = override(addCompression(), addAnalyzer(), addWebpackPlugin(new ProgressBarPlugin()));
+const splitChunks = () => (config) => {
+  config.optimization.splitChunks = {
+    chunks: "all",
+    minSize: 10000,
+    maxSize: 50000,
+    minChunks: 2,
+    maxAsyncRequests: 30,
+    maxInitialRequests: 30,
+    automaticNameDelimiter: "~",
+    cacheGroups: {
+      defaultVendors: {
+        test: /[\\/]node_modules[\\/]/,
+        priority: -10,
+        reuseExistingChunk: true,
+      },
+      default: {
+        minChunks: 2,
+        priority: -20,
+        reuseExistingChunk: true,
+      },
+      common: {
+        name: `chunk-common`,
+        minChunks: 2,
+        priority: -20,
+        chunks: "initial",
+        reuseExistingChunk: true, //如果当前块包含已经从主包中分离出来的模块，那么该模块将被重用，而不是生成新的模块
+      },
+    },
+  };
+
+  return config;
+};
+
+module.exports = override(addCompression(), addAnalyzer(), addWebpackPlugin(new ProgressBarPlugin()), splitChunks());
