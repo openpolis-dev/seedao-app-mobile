cdf78f89b99478649d2ace1ed6de3475d821c0a5
CatFlow <22841916+Meow711@users.noreply.github.com>
2024-01-15 12:38:03 +0800

    fix category styles

--- src/components/proposalCom/categoryTag.jsx
+++ src/components/proposalCom/categoryTag.jsx
@@ -3,12 +3,11 @@ import styled from 'styled-components';
 const CategoryTag = styled.div`
   display: inline-block;
   border-radius: 4px;
-  border: 1px solid var(--border-color-1);
-  color: var(--bs-body-color_active);
+  border: 1px solid var(--border-color-2);
+  color: var(--border-color-2);
   font-size: 12px;
-  padding: 0 16px;
-  line-height: 24px;
-  text-align: center;
+  padding: 0 6px;
+  line-height: 20px;
 `;
 
 export default CategoryTag;
