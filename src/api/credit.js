a292679d17ee311422aa1722b75079089fdbbebb
CatFlow <22841916+Meow711@users.noreply.github.com>
2024-05-27 19:55:11 +0800

    modified lend id display

--- src/api/credit.js
+++ src/api/credit.js
@@ -10,7 +10,7 @@ export const getVaultData = () => {
 const formatRecord = (item) => {
   return {
     lendId: item.lendId,
-    lendIdDisplay: String(88000 + Number(item.lendId)),
+    lendIdDisplay: String(8800000 + Number(item.lendId)),
     status: item.lendStatus,
     debtor: item.debtor,
     borrowAmount: Number(item.borrowAmount),
