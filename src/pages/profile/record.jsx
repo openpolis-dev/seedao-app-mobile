661f45ef881e2c1dd20c82d437ad9417e148e03d
AAA <7518647+wendychaung@users.noreply.github.com>
2025-08-01 21:23:53 +0800

    fix: update transfer & receive  see

--- src/pages/profile/record.jsx
+++ src/pages/profile/record.jsx
@@ -1,26 +1,31 @@
 import Layout from "../../components/layout/layout";
 import useQuerySNS from "../../hooks/useQuerySNS";
 import styled from "styled-components";
-import React, { useEffect, useState } from "react";
-
+import React, {useEffect, useMemo, useState} from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { useTranslation } from "react-i18next";
 import useToast from "hooks/useToast";
 
 import publicJs from "../../utils/publicJs";
 import {getSeeList} from "../../api/see";
 import dayjs from "dayjs";
+import store from "../../store";
+import {saveLoading} from "../../store/reducer";
 
 
 const CardBox = styled.div`
   min-height: 100%;
   padding: 0 20px;
+    .amount{
+        color:var( --primary-color)
+    }
 `;
 
 const UlBox = styled.ul`
     font-size: 12px;
 background: var(--background-color-2);
     margin-bottom: 20px;
-    padding: 10px;
+    padding: 15px;
     border-radius: 10px;
     box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.02);
   dl {
@@ -28,9 +33,8 @@ background: var(--background-color-2);
     align-items: center;
     justify-content: space-between;
       padding: 5px 0;
-    .title {
+    dt {
       color: #9a9a9a;
-      font-size: 14px;
     }
   }
     .flex{
@@ -53,6 +57,10 @@ export default function Record() {
     const { Toast, toast, showToast } = useToast();
 
 
+    // const hasMore = useMemo(() => {
+    //     console.log(list.length,total,list.length < total)
+    //     return list.length < total;
+    // }, [total, list]);
 
     const { getMultiSNS } = useQuerySNS();
 
@@ -71,54 +79,72 @@ export default function Record() {
 
     };
     const getList= async() =>{
-        const obj = {
-            page: pageCur,
-            size: pageSize,
-        };
+        try {
+              store.dispatch(saveLoading(true));
+            const obj = {
+                page: pageCur,
+                size: pageSize,
+            };
 
-        const rt = await getSeeList(obj);
-        console.log(rt);
+            const rt = await getSeeList(obj);
+            console.log(rt);
 
 
-        const { rows, page, size, total } = rt.data;
+            const { rows, page, size, total } = rt.data;
 
-        const _wallets = new Set();
-        rt.data.rows.forEach((r) => {
-            _wallets.add(r.from_user?.toLowerCase());
-            _wallets.add(r.to_user?.toLowerCase());
-        });
+            const _wallets = new Set();
+            rt.data.rows.forEach((r) => {
+                _wallets.add(r.from_user?.toLowerCase());
+                _wallets.add(r.to_user?.toLowerCase());
+            });
 
-        handleSNS(Array.from(_wallets));
-        setList(rows);
-        setPageSize(size);
-        setTotal(total);
-        setPageCur(page);
+            let arr = [...list,...rows];
+            handleSNS(Array.from(_wallets));
+            setList(arr);
+            setPageSize(size);
+            setTotal(total);
+            if(arr.length <total){
+                setPageCur(page + 1);
+            }
+
+        } catch(error) {
+          console.error(error)
+            toast.danger(`${error?.data?.msg || error?.code || error}`);
+        }finally {
+            store.dispatch(saveLoading(false));
+        }
+
     }
 
 
 
 
-    const handlePage = (num) => {
-        setPageCur(num + 1);
-    };
+    // const handlePage = (num) => {
+    //     setPageCur(num + 1);
+    // };
 
     const fromSns =(wallet) => {
         const name = snsMap[wallet?.toLowerCase()] || wallet;
         return name?.endsWith('.seedao') ? name : publicJs.AddressToShow(name, 4);
     };
 
-
-
-
     return<Layout
             noTab
             title={t("see.record")}
         >
-            <CardBox>
+            <CardBox  id="inner">
+                <InfiniteScroll
+                    scrollableTarget="inner"
+                    dataLength={list.length}
+                    next={getList}
+                    hasMore={list.length < total}
+                    loader={<></>}
+                >
+
                 {
                     list.map((item,index) =>( <UlBox key={index}>
                         <div className="flex">
-                            <div>{fromSns(item.amount)} SEE</div>
+                            <div className="amount">{fromSns(item.amount)} SEE</div>
                             <div>{dayjs(item.transaction_ts * 1000).format(`YYYY-MM-DD HH:mm:ss`)}</div>
                         </div>
                             <dl>
@@ -137,12 +163,13 @@ export default function Record() {
 
 
                         <dl>
-                            <dd>{fromSns(item.comment)}</dd>
+                            <dd>{item.comment}</dd>
                         </dl>
 
                         </UlBox>)
                     )
                 }
+                </InfiniteScroll>
             </CardBox>
             {Toast}
         </Layout>
